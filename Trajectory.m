70f44cbfd2c784a4f89cda52d195efe25f30bbe0
Rishu Mohanka <rishu.mohanka@gmail.com>
2020-07-15 00:11:00 -0400

    Trajectory abstract base class

---
+++ Trajectory.m
@@ -0,0 +1,28 @@
+classdef (Abstract) Trajectory
+    %TRAJECTORY - Abstract class as basis for different kinds of trajectory
+    %objects.
+    
+    %{
+    Properties - 
+    # of degrees of freedom/dimensions 
+    representation for the trajectory 
+    (and this depends on whatever subclasses implement the interface 
+    and how they represent the trajectory, for example, the multisegment planner 
+    trajectory uses a polynomial, but we want to account for trajectories that 
+    show the different angles like for the squeeguee trajectory and its rotation)
+    
+    Methods - getter, setter methods
+    %}  
+    
+    properties (GetAccess = private)
+        dimensions
+        trajectory
+    end
+    
+    methods (Abstract)
+        % obj is like the 'this' keyword in Python or Java 
+        dof = getDimensions(obj)
+        traj = getTrajectory(obj) 
+    end
+end
+
