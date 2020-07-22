44ab5ff7f5863cd9b40005baa4f572f67bea1d74
Rishu Mohanka <rishu.mohanka@gmail.com>
2020-07-22 00:51:26 -0400

    condition min num trajs >= 2

--- BlendedTrajPlanner.m
+++ BlendedTrajPlanner.m
@@ -18,7 +18,8 @@ classdef BlendedTrajPlanner < Trajectory
             use to eval positions, velocities, accelerations, jerks, etc.)
             and a column vector of dTs. And it must be alternating just in case 
             the user has multiple trajectory objects and sets of dTs for 
-            corresponding to them
+            corresponding to them. Assume there are at least 2 trajectory
+            objects given by the user
             
             dT is the time difference that our generated trajectory
             operates between, so if dT = 5 then our generated trajectory
@@ -51,70 +52,54 @@ classdef BlendedTrajPlanner < Trajectory
             dimensions)
             %}
             
-            % do the first trajectory
-            currentTrajObj = obj.cellArrayTrajs{1,1};
-            dimensionsCurrentTraj = currentTrajObj.dimensions;
-            currentdT = obj.dTs(1,1);
-            endT = currentTrajObj.times(end, 1);
-            t = [endT ; endT + currentdT];
-            trajs = [];
-            wp = {};
-            for i = 1:dimensionsCurrentTraj
-                trajs = [trajs ; currentTrajObj.getTrajectory(i)]; %#ok<*AGROW>
-            end
-            nextTrajs = [];
-            nextTrajObj = obj.cellArrayTrajs{1,2};
-            for i = 1:dimensionsCurrentTraj
-                nextTrajs = [nextTrajs ; nextTrajObj.getTrajectory(i)]; %#ok<*AGROW>
-            end
-            for i = 1:dimensionsCurrentTraj
-                currentTraj = trajs(i, 1:end);
-                collectTrajs = [collectTrajs ; currentTraj];
-                currentNextTraj = nextTrajs(i, 1:end);
-                
-                if size(obj.cellArrayTrajs) == 1
-                    wp{i, 1} = [polyval(currentTraj, t(1,1)) ; NaN];
-                elseif size(obj.cellArrayTrajs) > 1
-                    wp{i, 1} = [polyval(currentTraj, t(1,1)) ; polyval(currentNextTraj, t(2,1))];
+            for i = 2:size(obj.cellArrayTrajs, 2)-1
+                collectTrajs = [];
+                beforeTrajObj = obj.cellArrayTrajs{i-1,1};
+                dimensionsCurrentTraj = currentTrajObj.dimensions;
+                currentdT = obj.dTs(1,1);
+                endT = currentTrajObj.times(end, 1);
+                t = [endT ; endT + currentdT];
+                trajs = [];
+                wp = {};
+                for i = 1:dimensionsCurrentTraj
+                    trajectoryRow = currentTrajObj.getTrajectory(i);
+                    trajectoryRow = trajectoryRow(end - 7:end, 1).';
+                    trajs = [trajs ; trajectoryRow] %#ok<*AGROW>
                 end
-                
-                wp{i, 2} = ...
-                    [polyval(polyder(currentTraj), t(1,1)) ; 
-                    polyval(polyder(polyder(currentTraj)), t(1,1)) ;
-                    polyval(polyder(polyder(polyder(currentTraj))), t(1,1))];                
-                
-                if size(obj.cellArrayTrajs) == 1
-                    wp{i, 3} = [NaN ; NaN ; NaN];
-                elseif size(obj.cellArrayTrajs) > 1
+                afterTrajs = [];
+                afterTrajObj = obj.cellArrayTrajs{1,2};
+                for i = 1:dimensionsCurrentTraj
+                    afterTrajs = [afterTrajs ; afterTrajObj.getTrajectory(i)]; %#ok<*AGROW>
+                end
+                for i = 1:dimensionsCurrentTraj
+                    currentTraj = trajs(i, 1:end);
+                    collectTrajs = [collectTrajs ; currentTraj];
+                    if size(obj.cellArrayTrajs) > 1
+                        currentNextTraj = nextTrajs(i, 1:end);
+                    end
+
+                    wp{i, 1} = [polyval(currentTraj, t(1,1)) ; polyval(currentNextTraj, t(2,1))];
+
+                    wp{i, 2} = ...
+                        [polyval(polyder(currentTraj), t(1,1)) ; 
+                        polyval(polyder(polyder(currentTraj)), t(1,1)) ;
+                        polyval(polyder(polyder(polyder(currentTraj))), t(1,1))];                
+
+                    size(obj.cellArrayTrajs) > 1
                     wp{i, 3} = ...
                         [polyval(polyder(currentNextTraj), t(2,1)) ; 
                         polyval(polyder(polyder(currentNextTraj)), t(2,1)) ;
                         polyval(polyder(polyder(polyder(currentNextTraj))), t(2,1))];
                 end
-            end
-            genBlenTraj = MultiSegmentTrajPlanner(wp, t, dimensionsCurrentTraj);
-            genBlenTrajs = [];
-            for i = 1:dimensionsCurrentTraj
-                genBlenTrajs = [genBlenTrajs ; genBlenTraj.getTrajectory(i)];
-                collectTrajs = [collectTrajs ; genBlenTrajs];
-            end
+                celldisp(wp)
+                t
+                genBlenTraj = MultiSegmentTrajPlanner(wp, t, dimensionsCurrentTraj);
+                genBlenTrajs = [];
+                for i = 1:dimensionsCurrentTraj
+                    genBlenTrajs = [genBlenTrajs ; genBlenTraj.getTrajectory(i)];
+                    collectTrajs = [collectTrajs ; genBlenTrajs];
+                end
             
-            
-            % go in between
-            for i = 2:size(obj.cellArrayTrajs)-1
-                currentTrajObj = obj.cellArrayTrajs{1,i};
-                dimensionsCurrentTraj = currentTrajObj.dimensions;
-                
-            end
-            
-            % do the last trajectory
-            if size(obj.cellArrayTrajs) >= 2
-                currentTrajObj = obj.cellArrayTrajs{1,end};
-                dimensionsCurrentTraj = currentTrajObj.dimensions;
-                currentdT = obj.dTs(end,1);
-                startT = currentTrajObj.times(1, 1); % this is start of last trajectory/end of this one
-                
-                t = [endT ; endT + currentdT];
             end
             
         end
